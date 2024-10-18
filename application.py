065e73f805fdf9e9fe9960e30bf7b25d6deb1bec
Red-XO <dineshraja0063@gmail.com>
2024-10-18 16:22:44 +0530

    Update application.py

--- application.py
+++ application.py
@@ -9,6 +9,7 @@ from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 from helpers import errorPage, login_required, lookup, usd
+from dotenv import load_dotenv
 
 # Configure application
 application = Flask(__name__)
